eab548c6a2db018e4573ba0eca7967e5816d4500
Vetle Hjelmtvedt <vetle@hjelmtvedt.com>
2018-11-21 16:12:32 +0100

    Add dice roll and corresponding images

--- ludoapp/public/javascripts/game.js
+++ ludoapp/public/javascripts/game.js
@@ -1,7 +1,57 @@
 var main = function() {
     "use strict";
 
-    $("div.timeelapsed h3").text("Time Elapsed: 00:00");
 
+    let name = prompt("Please enter your name: ", "Player 1");
+    player.setName(name);
+    
+    $("div.player1 h3").text(name);
+    
+
+    $("#diceicon1").on("click", function (event) {
+        diceRoll();
+    });
+
+    
+}
+
+var diceRoll = function() {
+    var diceResult = Math.floor(Math.random() * 6) + 1;
+    $("#diceicon1").attr("src", "images/" + diceResult + ".png");
+       
+    return diceResult;
 };
+
+
+var player = ( function() {
+
+    let name = name;
+    let currentTurns = 0;
+    var figurinesOnTable = 0;
+    var currentPlayerTurn = 0;
+    var figurinesInHome = 4;
+    var figurinesInFinish = 0;
+    var playerID = 1;
+
+    return {
+        setName : function(nameIn) {
+            name = nameIn;
+        }
+    }
+
+
+
+
+})();
+
+var figurine = ( function() {
+    var isAbleToMove = false;
+    var position = 0;
+    var onAnotherPiece = true;
+    var killedByAnotherPiece = 0;
+    var color = "blue";
+    var ownedBy = 1;
+})();
+
+
 $(document).ready(main);
