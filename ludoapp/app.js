38dfd40d1e87e897495612b7388bbe58587441fe
Vetle Hjelmtvedt <vetle@hjelmtvedt.com>
2018-11-25 20:36:06 +0100

    Add routes

--- ludoapp/app.js
+++ ludoapp/app.js
@@ -1,8 +1,21 @@
 var express = require("express");
 var http = require("http");
-
+var indexRouter = require("./routes/index");
 var port = process.argv[2];
 var app = express();
 
+app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-http.createServer(app).listen(port);
+
+app.get("/play", indexRouter);
+
+//TODO: move to routes/index
+app.get("/", (req, res) => {
+    res.render("splash.ejs");
+});
+
+var server = http.createServer(app);
+
+
+
+server.listen(port);
