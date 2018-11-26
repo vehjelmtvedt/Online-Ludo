7335c6ad75ce088f8f5ee17f8b00113eb2d8e1f3
Vetle Hjelmtvedt <vetle@hjelmtvedt.com>
2018-11-26 22:22:28 +0100

    Add movement ability for all four figurines

--- ludoapp/public/javascripts/game.js
+++ ludoapp/public/javascripts/game.js
@@ -1,70 +1,33 @@
 var diceNumber = 0;
-var currentPosition = 0;
+var currentPosition1 = -1;
+var currentPosition2 = -1;
+var currentPosition3 = -1;
+var currentPosition4 = -1;
+var movesLeft = 0;
+
+var greenRoute = [document.getElementById("c7-2"), document.getElementById("c7-3"), document.getElementById("c7-4"), 
+    document.getElementById("c7-5"), document.getElementById("c7-6"), document.getElementById("c7-7")
+    , document.getElementById("c6-7"), document.getElementById("c5-7"), document.getElementById("c4-7")
+    , document.getElementById("c3-7"), document.getElementById("c2-7"), document.getElementById("c1-7")
+    , document.getElementById("c1-8"), document.getElementById("c1-9"), document.getElementById("c2-9")
+    , document.getElementById("c3-9"), document.getElementById("c4-9"), document.getElementById("c5-9")
+    , document.getElementById("c6-9"), document.getElementById("c7-9"), document.getElementById("c7-10")
+    , document.getElementById("c7-11"), document.getElementById("c7-12"), document.getElementById("c7-13")
+    , document.getElementById("c7-14"), document.getElementById("c7-15"), document.getElementById("c8-15")
+    , document.getElementById("c9-15"), document.getElementById("c9-14"), document.getElementById("c9-13")
+    , document.getElementById("c9-12"), document.getElementById("c9-11"), document.getElementById("c9-10")
+    , document.getElementById("c9-9"), document.getElementById("c10-9"), document.getElementById("c11-9")
+    , document.getElementById("c12-9"), document.getElementById("c13-9"), document.getElementById("c14-9")
+    , document.getElementById("c15-9"), document.getElementById("c15-8"), document.getElementById("c15-7")
+    , document.getElementById("c14-7"), document.getElementById("c13-7"), document.getElementById("c12-7")
+    , document.getElementById("c11-7"), document.getElementById("c10-7"), document.getElementById("c9-7")
+    , document.getElementById("c9-6"), document.getElementById("c9-5"), document.getElementById("c9-4")
+    , document.getElementById("c9-3"), document.getElementById("c9-2"), document.getElementById("c9-1")
+    , document.getElementById("c8-1"), document.getElementById("c8-2"), document.getElementById("c8-3")
+    , document.getElementById("c8-4"), document.getElementById("c8-5"), document.getElementById("c8-6")
+    , document.getElementById("c8-7")];
 
 
-
-var game = function(gameID) {
-    this.playerA = null;
-    this.playerB = null;
-    this.playerC = null;
-    this.playerD = null;
-    this.id = gameID;
-    this.gameState = "0 JOINED";
-    this.currentPlayer = null;
-    
-
-};
-
-game.prototype.transitionStates = {};
-game.prototype.transitionStates["0 JOINED"] = 0;
-game.prototype.transitionStates["1 JOINED"] = 1;
-game.prototype.transitionStates["2 JOINED"] = 2;
-game.prototype.transitionStates["3 JOINED"] = 3;
-game.prototype.transitionStates["4 JOINED"] = 4;
-game.prototype.transitionStates["PLAYER DONE"] = 5;
-game.prototype.transitionStates["PLAYER1"] = 6; //A won
-game.prototype.transitionStates["PLAYER2"] = 7; //A won
-game.prototype.transitionStates["PLAYER3"] = 8; //A won
-game.prototype.transitionStates["PLAYER4"] = 9; //A won
-game.prototype.transitionStates["ABORTED"] = 10;
-
-game.prototype.getCurrentPlayer() = function() {
-    return this.currentPlayer;
-};
-
-game.prototype.hasFourConnectedPlayers = function() {
-    return (this.gameState == "4 JOINED");
-};
-
-game.prototype.addPlayer = function(player) {
-    console.assert(p instanceof Object);
-
-    if (this.gameState != "1 JOINED" && this.gameState != "2 JOINED" && this.gameState != "3 JOINED" && this.gameState != "4 JOINED") {
-        return new Error("Lobby is full, current state is %s", this.gameState);
-    }
-
-    if (this.playerA == null) {
-        this.playerA = p;
-        return "A";
-    }
-
-    else if (this.playerB == null) {
-        this.playerB = p;
-        return "B";
-    }
-
-    else if (this.playerC == null) {
-        this.playerC = p;
-        return "C";
-    }
-
-    else {
-        this.playerD = p;
-        return "D";
-    }
-};
-
-module.exports = game;
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -89,21 +52,23 @@ function diceRoll() {
                 var rNumber = Math.floor(Math.random()*6+1);
                 diceIcon.src = `./images/${rNumber}.png`;
                 await sleep(100);
-                console.log(i);
+                
                 
             }
             diceIcon.src = `./images/${diceNumber}.png`;
         })();
+
+        movesLeft = 1;
 }
 
 
-function movePiece()
+function movePiece(cellID)
 {
+    if (movesLeft === 0) {
+        return;
+    }
     
-    
-    
-
-    var gridGreen = [document.getElementById("c7-2"), document.getElementById("c7-3"), document.getElementById("c7-4"), 
+    var greenRoute = [document.getElementById("c7-2"), document.getElementById("c7-3"), document.getElementById("c7-4"), 
     document.getElementById("c7-5"), document.getElementById("c7-6"), document.getElementById("c7-7")
     , document.getElementById("c6-7"), document.getElementById("c5-7"), document.getElementById("c4-7")
     , document.getElementById("c3-7"), document.getElementById("c2-7"), document.getElementById("c1-7")
@@ -125,31 +90,140 @@ function movePiece()
     , document.getElementById("c8-4"), document.getElementById("c8-5"), document.getElementById("c8-6")
     , document.getElementById("c8-7")];
 
+    //test if cellID clicked correspons to current position of any of the figurines. If so, move it. If not, return.
+    if (document.getElementById(cellID) === greenRoute[currentPosition1]) {
+        moveAnimation(currentPosition1 + diceNumber, currentPosition1, 1);
+        restorePositions(currentPosition1 + diceNumber, currentPosition2, currentPosition3, currentPosition4);
+        currentPosition1 += diceNumber;
+        return;
+    }
 
-    let previousPosition = currentPosition;
-    currentPosition += diceNumber;
+    else if (document.getElementById(cellID) === greenRoute[currentPosition2]) {
+        moveAnimation(currentPosition2 + diceNumber, currentPosition2, 2);
+        restorePositions(currentPosition1, currentPosition2 + diceNumber, currentPosition3, currentPosition4);
+        currentPosition2 += diceNumber;
+        return;
+    }
+
+    else if (document.getElementById(cellID) === greenRoute[currentPosition3]) {
+        moveAnimation(currentPosition3 + diceNumber, currentPosition3, 3);
+        restorePositions(currentPosition1, currentPosition2, currentPosition3 + diceNumber, currentPosition4);
+        currentPosition3 += diceNumber;
+        return;
+    }
+
+    else if (document.getElementById(cellID) === greenRoute[currentPosition4]) {
+        moveAnimation(currentPosition4 + diceNumber, currentPosition4, 4);
+        restorePositions(currentPosition1, currentPosition2, currentPosition3, currentPosition4 + diceNumber);
+        currentPosition4 += diceNumber;
+        return;
+    }
+
+    else {
+        return;
+    }
+    
     
 
     //movement animation
-
-    ( async function moveAnimation() {
-        for (let i = previousPosition; i <= currentPosition; i++) {
-            gridGreen[i].style.backgroundImage = "url('./images/figurine_blue.jpg')";
-
+    async function moveAnimation(currPos, prevPos, fNumber) {
+        for (let i = prevPos; i <= currPos; i++) {
+    
+            
+            greenRoute[i].style.backgroundImage = `url("./images/figurine_blue${fNumber}.jpg")`;
             await sleep(100);
-            gridGreen[i].style.backgroundImage = "";
+    
+    
+    
+            greenRoute[i].style.backgroundImage = "";
+            
             
         }
-        gridGreen[currentPosition].style.backgroundImage = "url('./images/figurine_blue.jpg')"; 
-        gridGreen[previousPosition].style.backgroundImage = ""; 
+        greenRoute[currPos].style.backgroundImage = `url("./images/figurine_blue${fNumber}.jpg")`;
+        greenRoute[prevPos].style.backgroundImage = ""; 
         
-    })();
-    
+    };
 
     
+    movesLeft = 0;
+
+    function restorePositions(pos1, pos2, pos3, pos4) {
+        console.log("restoring positions");
+        if (pos1 >= 0) {
+            console.log("Restored 1");
+            greenRoute[currentPosition1].style.backgroundImage = "url('./images/figurine_blue1.jpg')";
+        }
     
+        if (pos2 >= 0) {
+            console.log("Restored 2");
+            greenRoute[currentPosition2].style.backgroundImage = "url('./images/figurine_blue2.jpg')";
+        }
+    
+        if (pos3 >= 0) {
+            greenRoute[currentPosition3].style.backgroundImage = "url('./images/figurine_blue3.jpg')";
+        }
+    
+        if (pos4 >= 0) {
+            greenRoute[currentPosition4].style.backgroundImage = "url('./images/figurine_blue4.jpg')";
+        }
+    }
+
     
 }
+
+
+
+
+function movePieceFromHome(cellID) {
+    if (diceNumber !== 6) {
+        return;
+    }
+
+    if (movesLeft !== 0) {
+        if (cellID === "c3-3") {
+            document.getElementById(cellID).style.backgroundImage = "";
+            document.getElementById("c7-2").style.backgroundImage = "url('./images/figurine_blue1.jpg')";
+            currentPosition1 = 0;
+            console.log("Piece 1 moved from home");
+        }
+        else if (cellID === "c3-4") {
+            document.getElementById(cellID).style.backgroundImage = "";
+            document.getElementById("c7-2").style.backgroundImage = "url('./images/figurine_blue2.jpg')";
+            console.log("Piece 2 moved from home");
+            currentPosition2 = 0;
+        }
+        else if (cellID === "c4-3") {
+            document.getElementById(cellID).style.backgroundImage = "";
+            document.getElementById("c7-2").style.backgroundImage = "url('./images/figurine_blue3.jpg')";
+            console.log("Piece 3 moved from home");
+            currentPosition3 = 0;
+        }
+        else if (cellID === "c4-4") {
+            document.getElementById(cellID).style.backgroundImage = "";
+            document.getElementById("c7-2").style.backgroundImage = "url('./images/figurine_blue4.jpg')";
+            console.log("Piece 4 moved from home");
+            currentPosition4 = 0;
+        }
+        
+    }
+    else {
+        return;
+    }
+
+    
+    movesLeft = 0;
+}
+
+window.onload = function populateBoard() {
+    
+    document.getElementById("c3-4").style.backgroundImage = "url('./images/figurine_blue1.jpg')"; 
+    document.getElementById("c3-3").style.backgroundImage = "url('./images/figurine_blue2.jpg')"; 
+    document.getElementById("c4-3").style.backgroundImage = "url('./images/figurine_blue2.jpg')"; 
+    document.getElementById("c4-4").style.backgroundImage = "url('./images/figurine_blue3.jpg')"; 
+    
+};
+
+
 
 
 
