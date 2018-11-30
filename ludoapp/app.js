4eaac2a5ee7fe87b4a41bd0d4833d0ef1c243fc6
Vetle Hjelmtvedt <vetle@hjelmtvedt.com>
2018-11-30 14:18:47 +0100

    Add routes to app.js

--- ludoapp/app.js
+++ ludoapp/app.js
@@ -1,13 +1,11 @@
 var express = require("express");
 var http = require("http");
-var indexRouter = require("./routes/index");
 var port = process.argv[2];
 var app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
-app.get("/play", indexRouter);
 
 //TODO: move to routes/index
 app.get("/", (req, res) => {
